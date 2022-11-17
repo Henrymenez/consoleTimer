a950dd6b6e1c17661fb04961bfa29cfdc2c73f97
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-11-17 11:20:45 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+namespace Time_Alert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Worktime duration in minutes: ");
+            int WorkDuration = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Resttime duration in minutes: ");
+            int RestDuration = Convert.ToInt32(Console.ReadLine());
+           startTimer st = new startTimer();
+            st.consoleTimerCount(WorkDuration,RestDuration);
+            
+        }
+    }
+}
