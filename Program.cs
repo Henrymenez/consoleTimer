4ef4384baaba597383cb186712033724b1d509d5
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-11-23 21:46:31 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,12 +5,24 @@ namespace Time_Alert
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a Worktime duration in minutes: ");
-            int WorkDuration = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a Resttime duration in minutes: ");
-            int RestDuration = Convert.ToInt32(Console.ReadLine());
-           startTimer st = new startTimer();
-            st.consoleTimerCount(WorkDuration,RestDuration);
+            try
+            {
+                Console.WriteLine("Enter a Worktime duration in minutes: ");
+                int WorkDuration = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a Resttime duration in minutes: ");
+                int RestDuration = Convert.ToInt32(Console.ReadLine());
+                startTimer st = new startTimer();
+                st.consoleTimerCount(WorkDuration, RestDuration);
+            }catch(FormatException ex)
+            {
+                Console.WriteLine("Error in input format, please try again");
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
             
         }
     }
